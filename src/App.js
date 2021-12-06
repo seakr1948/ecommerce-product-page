f92348e008ba1d7cfd440545dab2511a69a2a3f3
seakr1948 <seakr1948@gmail.com>
2021-12-05 21:03:07 -0800

    uploading to check if website works as intended

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 
 import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Nav from './components/Nav';
+import LightBox from './components/LightBox';
 
 function App() {
   return (
-    <div className="App d-flex flex-column">
-      <div class="spinner-border text-primary" role="status">
-        <span class="visually-hidden">Loading...</span>
+    <div className="App d-flex flex-column justify-content-center align-items-center">
+      <div className="container p-0">
+        <Nav />
+        {/* <LightBox/>
+        <section className="product-description p-4">
+          <h1>Sneaker Company</h1>
+          <h2>Fall Limited Edition Sneakers</h2>
+          <p>These low-profile sneakers are your perfect casual wear companion. Featuring a 
+        durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+        
+        <div className="price d-flex flex-nowrap justify-content-between align-items-center">
+          <div className="d-flex align-items-center">
+            <p id="price-actual">$125.00</p>
+            <p id="discount" className="px-2">50%</p>
+          </div>
+          <div>
+          <p id="price-msrp">250.00%</p>
+          </div>
+          
+        </div>
+        </section> */}
+        
       </div>
-      <div class="spinner-border text-secondary" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div class="spinner-border text-success" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div class="spinner-border text-danger" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div class="spinner-border text-warning" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div class="spinner-border text-info" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div class="spinner-border text-light" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
-      <div class="spinner-border text-dark" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
+      
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Cart from '../images/icon-cart.svg'
+import Avatar from "../images/image-avatar.png"
+import Menu from "../images/icon-menu.svg"
+
+const Nav = () => {
+    return (
+        <div>
+            <nav className="nav-bar px-2 py-4 d-flex justify-content-between align-items-center">
+                <div className="menu-logo d-flex align-items-center">
+                    <img className="menu " src={Menu} alt="menu" />
+                    <h1 className="">Sneakers</h1>
+                </div>
+                <div className="cart-avatar d-flex align-items-center">
+                    <img className="cart" src={Cart} alt="cart" srcset="" />
+                    <img className="avatar" src={Avatar} alt="Avatar" srcset="" />
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
