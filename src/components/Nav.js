40a04354f1340ee9d225fbc421488f3c7f40b95f
seakr1948 <seakr1948@gmail.com>
2021-12-06 08:56:28 -0800

    Revert "took off imports"
    This reverts commit 1532d88a785834372c1149f9654ffc966ca68482.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,18 @@
 
-
+import Cart from '../images/icon-cart.svg'
+import Avatar from '../images/image-avatar.png'
+import Menu from '../images/icon-menu.svg'
 
 const Nav = () => {
     return (
             <nav className="nav-bar px-2 py-4 d-flex justify-content-between align-items-center">
                 <div className="menu-logo d-flex align-items-center">
-                    <img className="menu " src="" alt="menu" />
+                    <img className="menu " src={Menu} alt="menu" />
                     <h1 className="outline">Sneakers</h1>
                 </div>
                 <div className="cart-avatar d-flex align-items-center">
-                    <img className="cart" src="" alt="cart" srcset="" />
-                    <img className="avatar" src="" alt="Avatar" srcset="" />
+                    <img className="cart" src={Cart} alt="cart" srcset="" />
+                    <img className="avatar" src={Avatar} alt="Avatar" srcset="" />
                 </div>
             </nav>
     )
